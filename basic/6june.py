da373a7bb84e4d93bc1fbc78a42df0e2aa76305e
dishant1123 <dishantshah1417@gmail.com>
2026-06-06 16:46:09 +0530

    basic

---
+++ basic/6june.py
@@ -0,0 +1,30 @@
+"""
+notes : 
+
+        .py            .ipynb
+1.   run  whole file   run cellwise 
+2.   folder            drive 
+
+why python  important  for analysis  ??
+
+1. data cleaning 
+2. data  manupulation 
+3. data  visualization 
+4. data  analysis
+"""
+
+"""
+print("my name is ram.")
+print("live  in india.")
+print("age  in 66")
+"""
+# comment : 
+"""
+1. single line  comment  : # 
+2. multi line   comment  :  ''' '''   or  """ """   
+"""
+
+print("my name is ram.")
+print("live  in india.")
+print("age  in 66")
+print("love pizza")
