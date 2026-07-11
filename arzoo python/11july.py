0d04dcbfed93a6819b6ffac82485e6d0dbdb8073
dishant1123 <dishantshah1417@gmail.com>
2026-07-11 15:34:24 +0530

    ....

--- arzoo python/11july.py
+++ arzoo python/11july.py
@@ -1,4 +1,22 @@
 """
-mwf  : 11 - 8 11-12  12 -1.30  3-5  7-8
-TTS : 8.30 -5    2-3.30 
+python  : object oriented languges 
+
+use : 
+1. data  cleaning  
+2. data analysis 
+3. hacking 
+4. GUI :graphical user interface 
+5. gaming  development
+6. AI - ML : 
+
+software :  VS , Pycharm , Spyder , Anaconda,google colab,jypter notebook 
+
+python  : interpreted languages
+
+
 """
+
+print("my name is  dishant.")
+print("love pizza")
+print("live in india.")
+print("live in ahmedabad.")
