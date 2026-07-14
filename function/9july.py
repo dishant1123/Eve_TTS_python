d6ca32a0cd573614febda88aedd7628aee9aaedf
dishant1123 <dishantshah1417@gmail.com>
2026-07-14 15:59:43 +0530

    ....

--- function/9july.py
+++ function/9july.py
@@ -1,5 +1,4 @@
-"""
-bank app  : 
+"""bank app  : 
 
 1. create  account :
     create  a  user name  : dishant 
@@ -16,109 +15,161 @@ bank app  :
 4. exit 
 """
 
-account ={}
-
+account = {}
 def create_password(password):
-    
-    if len(password)>=8 :
-        lower =False
-        upper =False
-        digit =False
-        special =False
-        
-        special_char =["@","#","$","%","^","&","*"]
-        
-        for  i in password :
-            if  i.isupper() :
-                upper =True
-            elif  i.islower() :
-                lower =True
-            elif  i.isdigit() :
-                digit =True
-            elif  i in special_char :
-                special =True
-        
-        if  upper and lower and digit and special :
-            return True
-        else :
-            return False
-    else :
-        return False
+    if len(password) >= 8:
+        lower = False
+        upper = False
+        digit = False
+        special = False
+
+        special_char = "@#$%^&*"
+
+        for ch in password:
+
+            if ch.isupper():
+                upper = True
+
+            elif ch.islower():
+                lower = True
+
+            elif ch.isdigit():
+                digit = True
+
+            elif ch in special_char:
+                special = True
+
+        return upper and lower and digit and special
+    return False
 
 def create_account():
-    
-    print("----------create account ----------")
-    
-    username =input("enter username : ")
-    password =input("enter password : ")
-    
-    if  create_password(password) :
-        account[username]=password
-        print("account created successfully")
-    else :
-        print("password is not valid")
-        
-def login():
-    username =input("enter username : ")
-    password =input("enter password : ")
-    
-    if username in account :
-        if account[username]==password :
-            print("login success")
-            # account[username]=25000 
-        else: 
-            print("password is not valid")
-    else :
-        print("username is not valid") 
-        
-def deposit(username):
-    amount =int(input("enter amount : "))
-    
-    if amount<0 :
-        print("amount should be positive")
-    else :
-        if username in account :
-            account[username]['balance']+=amount
-            print("deposit success")
-    
-        
-def withdraw(username):
-    amount =int(input("enter amount : "))  # 20000
-    if amount<0 :
-        print("amount should be positive")
-    else :
-        if username in account :
-            if account[username]['balance']>=amount :# 5000 > 20000 
-                account[username]['balance']-=amount
-                print("withdraw success")
-        else :
-            print("username is not valid")
-        
-    
-def check_balance(username):
-    print("your  final balance is  : ",account[username]['balance'])    
-def main():
-    while True :
-        print("1. create account")
-        print("2. login")
-        print("3. deposit")
-        print("4. withdraw")
-        print("5. check balance")
-        print("6. exit")
-        choice =int(input("enter choice : "))
-        match choice :
-            case 1 :
-                create_account()
-            case 2 :
-                login()
-            case 3 :
-                deposit()
-            case 4 :
-                withdraw()
-            case 5 :
-                check_balance()
-            case 6 :
-                break
-                
-main()
 
+    print("\n------ Create Account ------")
+    username = input("Enter Username : ")
+    if username in account:
+        print("Username already exists.")
+        return
+    password = input("Enter Password : ")
+    if create_password(password):
+        account[username] = {
+            "password": password,
+            "balance": 25000
+        }
+        print("Account Created Successfully.")
+        print("Initial Balance : ₹25000")
+    else:
+        print("Invalid Password")
+
+
+def login():
+
+    print("\n------ Login ------")
+
+    username = input("Enter Username : ")
+    password = input("Enter Password : ")
+
+    if username in account:
+
+        if account[username]["password"] == password:
+
+            print("Login Successful")
+            return username
+
+        else:
+            print("Incorrect Password")
+
+    else:
+        print("Username Not Found")
+
+    return None
+
+
+def deposit(username):
+
+    amount = int(input("Enter Deposit Amount : "))
+
+    if amount <= 0:
+        print("Invalid Amount")
+        return
+
+    account[username]["balance"] += amount
+
+    print("Deposit Successful")
+    print("Current Balance :", account[username]["balance"])
+
+
+def withdraw(username):
+
+    amount = int(input("Enter Withdraw Amount : "))
+
+    if amount <= 0:
+        print("Invalid Amount")
+        return
+
+    if account[username]["balance"] - amount >= 10000:
+
+        account[username]["balance"] -= amount
+
+        print("Withdraw Successful")
+        print("Remaining Balance :", account[username]["balance"])
+
+    else:
+        print("Minimum Balance of ₹10000 must be maintained.")
+
+
+def check_balance(username):
+
+    print("Current Balance :", account[username]["balance"])
+
+
+def main():
+
+    logged_user = None
+
+    while True:
+
+        print("\n========= BANK MENU =========")
+        print("1. Create Account")
+        print("2. Login")
+        print("3. Deposit")
+        print("4. Withdraw")
+        print("5. Check Balance")
+        print("6. Exit")
+
+        choice = int(input("Enter Choice : "))
+
+        match choice:
+
+            case 1:
+                create_account()
+
+            case 2:
+                logged_user = login()
+
+            case 3:
+                if logged_user:
+                    deposit(logged_user)
+                else:
+                    print("Please login first.")
+
+            case 4:
+                if logged_user:
+                    withdraw(logged_user)
+                else:
+                    print("Please login first.")
+
+            case 5:
+                if logged_user:
+                    check_balance(logged_user)
+                else:
+                    print("Please login first.")
+
+            case 6:
+                print("Thank You")
+                break
+
+            case _:
+                print("Invalid Choice")
+
+
+main()
